b636edf67f8a0f6d0a2fa702df34c91c760cda53
Ismail Khan <ismail.khan02@gmail.com>
2026-03-04 14:06:27 +0000

    Removed key-pair requirement

--- terraform_configs/main.tf
+++ terraform_configs/main.tf
@@ -1,13 +1,16 @@
 # main.tf
 resource "aws_instance" "lamp_instance" {
-  ami                    = data.aws_ami.amazon_linux_2023.id 
+  ami                    = data.aws_ami.amazon_linux_2023.id
   instance_type          = "t2.micro"
-  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   subnet_id              = aws_subnet.public-subnet-1.id
   user_data              = file("install_lamp.sh")
+  iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+
+
 
   tags = {
     Name = "LAMP_Server"
   }
 }
+
