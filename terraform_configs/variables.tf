84d89cf5125cb92d5549d1b27a1ddf94e9efbdad
Ismail Khan <ismail.khan02@gmail.com>
2026-03-04 16:21:24 +0000

    Added email for SNS notifications

--- terraform_configs/variables.tf
+++ terraform_configs/variables.tf
@@ -49,3 +49,8 @@ variable "db_password" {
   type        = string
   sensitive   = true
 }
+
+variable "alert_email" {
+  description = "Email address to receive CloudWatch alarm notifications"
+  type        = string
+}
