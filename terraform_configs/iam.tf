5cfb57ee6480a43fc1b88275e9bc5cae9e391706
Ismail Khan <ismail.khan02@gmail.com>
2026-03-04 15:52:39 +0000

    Added role for secrets manager

--- terraform_configs/iam.tf
+++ terraform_configs/iam.tf
@@ -32,3 +32,8 @@ resource "aws_iam_instance_profile" "ssm_profile" {
   role = aws_iam_role.ssm_role.name
 }
 
+resource "aws_iam_role_policy_attachment" "secrets_attach" {
+  role       = aws_iam_role.ssm_role.name
+  policy_arn = "arn:aws:iam::aws:policy/SecretsManagerReadWrite"
+  
+}
