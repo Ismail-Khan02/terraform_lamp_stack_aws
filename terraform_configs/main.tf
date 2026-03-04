56d8e85eea89ed3d7e5403e229113a21316a6d27
Ismail Khan <ismail.khan02@gmail.com>
2026-03-04 22:02:50 +0000

    Added variable to instance_type

--- terraform_configs/main.tf
+++ terraform_configs/main.tf
@@ -1,7 +1,7 @@
 # main.tf
 resource "aws_instance" "lamp_instance" {
   ami                    = data.aws_ami.amazon_linux_2023.id
-  instance_type          = "t2.micro"
+  instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.web_sg.id]
   subnet_id              = aws_subnet.public-subnet-1.id
   user_data              = file("install_lamp.sh")
