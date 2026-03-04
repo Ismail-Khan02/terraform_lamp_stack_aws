11f269857a8fb8329c7b5cf198e51eec095a0541
Ismail Khan <ismail.khan02@gmail.com>
2026-03-04 15:54:02 +0000

    Added port 443 for HTTPS

--- terraform_configs/sg.tf
+++ terraform_configs/sg.tf
@@ -1,7 +1,7 @@
 # Create a security group allowing HTTP and SSH access
 resource "aws_security_group" "web_sg" {
   name        = "web_server_sg"
-  description = "Allow HTTP and SSH traffic"
+  description = "Allow HTTP and HTTPS traffic"
   vpc_id      = aws_vpc.main.id
 
   ingress {
@@ -12,11 +12,25 @@ resource "aws_security_group" "web_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  ingress {
+    description = "HTTPS from anywhere"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "web-server-sg"
+    Environment = var.environment
   }
 }
 
